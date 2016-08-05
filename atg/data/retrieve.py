b24b6a9008f80f9a7aabc36f65d4cae5560cf96e
Max Chang <maxwchang@gmail.com>
2016-08-05 16:11:22 -0700

    Can download data from Ensembl using XML specifications

--- atg/data/retrieve.py
+++ atg/data/retrieve.py
@@ -4,9 +4,48 @@ Assemble genomic data from various sources (e.g. UCSC, ENSEMBL) and organize the
 
 import os
 import sys
+import urllib.parse
 import urllib.request
+import urllib.error
 import atg.config
 import atg.data
+
+
+def fetch_ensembl(xml_filename, output_filename):
+    """
+    retrieve data from Ensembl using XML specifications (e.g. for gene annotation or Gene Ontology sets)
+
+    :param xml_filename:
+    :param output_filename:
+    :return:
+    """
+    input_file = open(xml_filename)
+    definition = ''.join(input_file.readlines())
+    query = urllib.parse.quote(definition)
+    url = 'http://www.ensembl.org/biomart/martservice?query=' + query
+    return fetch_url(url, output_filename)
+
+
+def fetch_url(url, path, overwrite=False):
+    """
+    Download specified url to destination path, returning True if successful. Will not overwrite existing files by
+    default.
+    :param url:
+    :param path:
+    :param overwrite:
+    :return:
+    """
+    if not overwrite and os.path.exists(path):
+        return False
+
+    try:
+        urllib.request.urlretrieve(url, filename=path)
+
+    except urllib.error.HTTPError as error:
+        print("Could not retrieve %s: %s [HTTP Error %s]" % (url, error.reason, error.code), file=sys.stderr)
+        return False
+
+    return True
 
 
 class ATGDataTracker:
@@ -27,6 +66,7 @@ class ATGDataTracker:
         Fetch all data files for a specified genome
 
         :param genome:
+        :param overwrite:
         :return:
         """
 
@@ -37,28 +77,8 @@ class ATGDataTracker:
         for filename, base_url in atg.data.genome_data.items():
             current_url = base_url.format(genome=genome)
             current_path = os.path.join(self.genome_path[genome], filename)
-            fetch_status = self._fetch_url(current_url, current_path, overwrite=False)
+            fetch_status = fetch_url(current_url, current_path, overwrite=overwrite)
 
-    def _fetch_url(self, url, path, overwrite=False):
-        """
-        Download specified url to destination path, returning True if successful. Will not overwrite existing files by
-        default.
-        :param url:
-        :param path:
-        :param overwrite:
-        :return:
-        """
-        if not overwrite and os.path.exists(path):
-            return False
-
-        try:
-            urllib.request.urlretrieve(url, filename=path)
-
-        except urllib.error.HTTPError as error:
-            print("Could not retrieve %s: %s [HTTP Error %s]" % (url, error.reason, error.code), file=sys.stderr)
-            return False
-
-        return True
 
 if __name__ == '__main__':
     tracker = ATGDataTracker()
