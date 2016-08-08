b29529b88b1dbe9f1c770d50efc3eeaae43ab474
Max Chang <maxwchang@gmail.com>
2016-08-08 15:48:36 -0700

    Allow creation of unstranded tracks.
    - Added option in hub configuration yaml file -- library. This can be either "rf" or "unstranded"
    - Added tests for an unstranded data set.
    - atg driver allows specification of library type for setup subcommand.

--- tests/test_coverage.py
+++ tests/test_coverage.py
@@ -4,19 +4,50 @@ import tempfile
 import csv
 from atg.quantification import coverage
 
+UNSTRANDED_SE_RNASEQ_FILE = os.path.join(os.path.dirname(__file__), 'data', 'rnaseq_unstranded_SE.bam')
+STRANDED_PE_RNASEQ_FILE = os.path.join(os.path.dirname(__file__), 'data', 'rnaseq.bam')
 
-RNASEQ_FILE = os.path.join(os.path.dirname(__file__), 'data', 'rnaseq.bam')
-
-
-class CoverageCalculatorTest(unittest.TestCase):
+class UnstrandedCoverageCalculatorTest(unittest.TestCase):
     def setUp(self):
-        self.coverage_calculator = coverage.CoverageCalculator(RNASEQ_FILE)
+        self.coverage_calculator = coverage.UnstrandedCoverageCalculator(UNSTRANDED_SE_RNASEQ_FILE)
+
+    def test_read_count(self):
+        self.assertEqual(self.coverage_calculator.mapped_read_count, 1224)
+
+    def test_bedgraph(self):
+        """
+        all entries should be on chromosome 11
+        forward file should have only positive entries in column 4, reverse file should have only negative
+        """
+        with tempfile.TemporaryDirectory() as working_directory:
+            forward_filename = os.path.join(working_directory, "forward.bedgraph")
+            self.coverage_calculator.write_bedgraph(forward_filename)
+
+            for entry in csv.reader(open(forward_filename), delimiter='\t'):
+                self.assertEqual(entry[0], 'chr11')
+                self.assertGreater(float(entry[3]), 0)
+
+    def test_bigwig(self):
+        """
+        just check that files were created
+        TODO: use checksum?
+        """
+        with tempfile.TemporaryDirectory() as working_directory:
+            forward_filename = os.path.join(working_directory, "forward.bigwig")
+            self.coverage_calculator.write_bigwig(forward_filename, 'hg38')
+
+            os.path.exists(forward_filename)
+
+
+class StrandedCoverageCalculatorTest(unittest.TestCase):
+    def setUp(self):
+        self.coverage_calculator = coverage.StrandedCoverageCalculator(STRANDED_PE_RNASEQ_FILE)
 
     def test_read_count(self):
         """
         test file contains 198 uniquely mapped R1 reads
         """
-        self.assertEqual(self.coverage_calculator.mapped_read_count, 198)
+        self.assertEqual(self.coverage_calculator.mapped_read_count, 196)
 
     def test_bedgraph(self):
         """
