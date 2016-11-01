11470fd67165f17ff45589f97431693011cc33d0
Max Chang <maxwchang@gmail.com>
2016-11-01 11:12:37 -0700

    Added voom-like count transformation (#12)

--- atg/quantification/normalization.py
+++ atg/quantification/normalization.py
@@ -10,8 +10,16 @@ def cpm_normalization(count_df):
 
 
 def _tmm_norm_factor_single(obs, ref, log_ratio_trim=0.3, sum_trim=0.05, weighting=True, a_cutoff=-1e10):
-    if all(obs == ref):
-        return 1
+    """
+
+    :param obs:
+    :param ref:
+    :param log_ratio_trim:
+    :param sum_trim:
+    :param weighting:
+    :param a_cutoff:
+    :return:
+    """
 
     obs_sum = obs.sum()
     ref_sum = ref.sum()
@@ -45,6 +53,11 @@ def _tmm_norm_factor_single(obs, ref, log_ratio_trim=0.3, sum_trim=0.05, weighti
 
 
 def tmm_norm_factor(count_df):
+    """
+
+    :param count_df:
+    :return:
+    """
     count_nonzero = (count_df.where(count_df > 0)
                      .dropna(how="all").fillna(0))
 
@@ -70,6 +83,32 @@ def tmm_normalization(count_df):
     return normalized_df
 
 
+def voom(count_df):
+    """
+    calculate log2 cpm on TMM-normalized data, as in voom. Further calculation of the dispersion trend would be used
+    in linear modeling.
+
+    :param count_df:
+    :return: a log2, normalized count dataframe
+    """
+
+    lib_size = count_df.sum() * tmm_norm_factor(count_df)
+    log_count_df = np.log2(count_df / (lib_size + 1) * 1e6)
+
+    # further calculations for modeling dispersion trend
+    # row-wise mean and SD
+    # gene_mean = count_df.mean(axis=1)
+    # gene_sd = count_df.std(axis=1)
+    #
+    # # fit lowess trend to sqrt-standard-deviations by log-count-size
+    # sx = gene_mean + (np.log2(lib_size+1)-np.log2(1e6)).mean()
+    # sy = np.sqrt(gene_sd)
+    #
+    # lowess_fit = sm.nonparametric.lowess(sy, sx, frac=0.5)
+
+    return log_count_df
+
+
 def normalize_counts(namespace):
     count_df = pandas.read_csv(namespace.count_filename, index_col=namespace.index, delimiter=namespace.delimiter)
     if namespace.method == 'TMM':
@@ -77,6 +116,12 @@ def normalize_counts(namespace):
 
     elif namespace.method == 'CPM':
         normalized_df = cpm_normalization(count_df)
+
+    elif namespace.method == 'voom':
+        normalized_df = voom(count_df)
+
+    else:
+        normalized_df = count_df
 
     normalized_df.to_csv(namespace.output_filename, sep=namespace.delimiter)
 
@@ -86,9 +131,10 @@ def setup_subparsers(subparsers):
 
     normalization_parser.add_argument('count_filename', help="delimited file containing read counts")
     normalization_parser.add_argument('output_filename', help="")
-    normalization_parser.add_argument('-m', '--method', default="TMM", choices=['TMM', 'CPM'],
+    normalization_parser.add_argument('-m', '--method', default="TMM", choices=['TMM', 'CPM', 'voom'],
                                       help="TMM: weighted Trimmed Mean of M-values; "
-                                           "CPM: Counts Per Million reads")
+                                           "CPM: Counts Per Million reads"
+                                           "voom: log2 count after TMM")
     normalization_parser.add_argument('-i', '--index', default=0, type=int,
                                       help="numerical position of the ID column")
     normalization_parser.add_argument('-d', '--delimiter', default=",", help='Text delimiter, e.g. ","')
--- tests/test_normalization.py
+++ tests/test_normalization.py
@@ -9,7 +9,7 @@ RNASEQ_COUNT_FILE = os.path.join(os.path.dirname(__file__), 'data', 'rnaseq.csv'
 EDGER_NORM_FACTOR = [0.9632356, 0.9739227, 0.9621926, 0.9607130, 1.1352795, 1.0157446]
 EDGER_TMM_VCP = [501.1333611, 540.4635384, 567.8398203, 345.3321018, 316.5981401, 377.8790519]
 EDGER_CPM_VCP = [482.7128, 526.3641, 546.3750, 331.7619, 359.4104, 383.8492]
-
+LIMMA_VOOM_VCP = [8.969148, 9.078111, 9.149407, 8.431966, 8.306551, 8.561956]
 
 class CountNormalizationTest(unittest.TestCase):
     def setUp(self):
@@ -26,6 +26,10 @@ class CountNormalizationTest(unittest.TestCase):
     def test_cpm_normalization(self):
         normalized_count = atg.quantification.normalization.cpm_normalization(self.read_count_df)
         numpy.testing.assert_almost_equal(EDGER_CPM_VCP, normalized_count.ix['VCP', :], decimal=4)
+
+    def test_voom_transformation(self):
+        transformed_count = atg.quantification.normalization.voom(self.read_count_df)
+        numpy.testing.assert_almost_equal(LIMMA_VOOM_VCP, transformed_count.ix['VCP', :], decimal=3)
 
 if __name__ == '__main__':
     unittest.main()
