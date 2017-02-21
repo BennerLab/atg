7b59a668309f2f3bf19d9584a052b06b95265b4d
Max Chang <maxwchang@gmail.com>
2017-02-21 13:50:06 -0800

    Go enrichment (#15)
    * Implemented simple GO enrichment for single category.
    
    * Implemented enrichment across all categories
    
    * Added plotting for enrichment analysis and updated tests
    
    * Updated Travis configuration to allow GUI
    
    * Added p-value correction (BH method) and iterative enrichment testing
    
    * Implemented simple GO enrichment for single category.
    
    * Implemented enrichment across all categories
    
    * Added plotting for enrichment analysis and updated tests
    
    * Updated Travis configuration to allow GUI
    
    * Added p-value correction (BH method) and iterative enrichment testing

--- atg/stats/enrich.py
+++ atg/stats/enrich.py
@@ -1,4 +1,117 @@
-import scipy.stats
+import pandas
+import numpy
+from scipy.stats import hypergeom
+
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+LOG_P_VALUE_MINIMUM = -40.0
+LOG10_FACTOR = 1.0 / numpy.log(10)
 
 
-scipy.stats.hypergeom.logsf()
+def enrichment_significance(term_row):
+    return hypergeom.logsf(term_row['hit_count'], term_row['universe'], term_row['term_count'], term_row['list_size'])
+
+
+def p_adjust_bh(p, n=None):
+    """Benjamini-Hochberg p-value correction for multiple hypothesis testing."""
+
+    p_array = numpy.asfarray(p)
+
+    if n:
+        p_array = numpy.append(p_array, numpy.ones(n - len(p)))
+
+    by_descend = p_array.argsort()[::-1]
+    by_orig = by_descend.argsort()
+    steps = float(len(p_array)) / numpy.arange(len(p_array), 0, -1)
+    q = numpy.minimum(1, numpy.minimum.accumulate(steps * p_array[by_descend]))
+
+    return q[by_orig][0:len(p)]
+
+
+class EnrichmentCalculator:
+    """
+    calculate stats on statistical enrichment on gene categories, e.g. Gene Ontology
+    """
+
+    def __init__(self, gene_term_file, term_definition_file, maximum_size=1000, minimum_size=3):
+        # TODO: filter by evidence code
+        gene_term_df = (pandas.read_csv(gene_term_file)
+                        .dropna()
+                        .drop_duplicates(['Ensembl Gene ID', 'GO Term Accession']))
+
+        term_count = gene_term_df['GO Term Accession'].value_counts()
+        relevant_terms = term_count[(term_count <= maximum_size) & (term_count >= minimum_size)]
+
+        self.gene_term_df = gene_term_df[gene_term_df['GO Term Accession'].isin(relevant_terms.index.values)]
+        self.term_count = self.gene_term_df['GO Term Accession'].value_counts()
+        self.universe = self.gene_term_df['Ensembl Gene ID'].nunique()
+
+        self.term_description = pandas.read_csv(term_definition_file).dropna()
+
+    def get_single_enrichment(self, gene_list, term, gene_universe=15000):
+        term_df = self.gene_term_df[self.gene_term_df['GO Term Accession'] == term]
+        n = len(term_df)
+        x = sum(term_df['Ensembl Gene ID'].isin(gene_list))  # matched genes
+        N = len(gene_list)
+
+        return hypergeom.logsf(x, gene_universe, n, N)
+
+    def get_all_enrichment(self, gene_list):
+        enrichment_df = pandas.DataFrame()
+        enrichment_df['term_count'] = self.term_count
+        enrichment_df['hit_count'] = (self.gene_term_df[self.gene_term_df['Ensembl Gene ID'].isin(gene_list)]
+                                                       ['GO Term Accession'].value_counts().astype(int))
+        enrichment_df.dropna(inplace=True)
+
+        enrichment_df['universe'] = self.universe
+        enrichment_df['list_size'] = len(gene_list)
+        enrichment_df['log_pvalue'] = enrichment_df.apply(enrichment_significance, axis=1, reduce=True) * LOG10_FACTOR
+
+        # replace negative infinite p-values with a small constant
+        enrichment_df['log_pvalue'].replace(-numpy.inf, LOG_P_VALUE_MINIMUM, inplace=True)
+        enrichment_df['log_adjusted_pvalue'] = numpy.log10(p_adjust_bh(numpy.power(10, enrichment_df['log_pvalue']),
+                                                                       n=len(self.term_description)))
+        return enrichment_df.sort_values(['log_adjusted_pvalue', 'hit_count'], ascending=[True, False])
+
+    def iterative_enrichment(self, gene_list, adj_log_p_value_threshold=-2.0):
+        current_gene_set = set(gene_list)
+        print(len(current_gene_set))
+
+        while len(current_gene_set) > 0:
+            current_enrichment_df = self.get_all_enrichment(current_gene_set)
+            # print(current_enrichment_df.head(10))
+            most_enriched_term_row = current_enrichment_df.iloc[0]
+            if most_enriched_term_row['log_adjusted_pvalue'] > adj_log_p_value_threshold:
+                break
+
+            most_enriched_term = most_enriched_term_row.name
+            print(most_enriched_term, self.term_description.loc[self.term_description['GO Term Accession'] == most_enriched_term, 'GO Term Name'])
+
+            current_term_genes = self.gene_term_df.loc[self.gene_term_df['GO Term Accession'] == most_enriched_term,
+                                                       'Ensembl Gene ID']
+            # remove genes present in most enriched term
+            current_gene_set.difference_update(current_term_genes.values)
+            print(len(current_term_genes.values), len(current_gene_set))
+
+    def plot_enrichment_single(self, gene_list, output_filename=None, log_pvalue_threshold=-2.0, num_terms=20,
+                               iterative=False):
+
+        annotated_enrichment_df = (self.get_all_enrichment(gene_list)
+                                       .merge(self.term_description, left_index=True, right_on="GO Term Accession")
+                                       .query('log_adjusted_pvalue < @log_pvalue_threshold')
+                                       .sort_values('log_adjusted_pvalue')
+                                       .iloc[0:num_terms]
+                                       .assign(display_log_pvalue=lambda x: x.log_adjusted_pvalue*-1))
+
+        sns.set(style='whitegrid')
+        plt.figure()
+        plot = sns.stripplot(x='display_log_pvalue', y='GO Term Name', data=annotated_enrichment_df,
+                             orient='h', edgecolor='gray', palette="Reds_r")
+        plot.set_xlabel(r'$-log_{10}(p)$')
+        plot.set_ylabel('')
+        plot.xaxis.grid(False)
+        plot.yaxis.grid(True)
+        sns.despine(left=True, bottom=True)
+        plt.tight_layout()
+        plt.savefig("/tmp/test.pdf")
---
+++ tests/test_enrichment.py
@@ -0,0 +1,70 @@
+import unittest
+import os
+import atg.stats.enrich
+import numpy
+
+# Genes present in GO:0007259
+SAMPLE_ENSEMBL_GENE_LIST = ['ENSG00000159110', 'ENSG00000185338', 'ENSG00000183709', 'ENSG00000182393',
+                            'ENSG00000126561', 'ENSG00000197110', 'ENSG00000170677', 'ENSG00000134470',
+                            'ENSG00000008710', 'ENSG00000124356', 'ENSG00000113494', 'ENSG00000123609',
+                            'ENSG00000120833', 'ENSG00000096968', 'ENSG00000168610', 'ENSG00000198730',
+                            'ENSG00000115415', 'ENSG00000112964', 'ENSG00000121807', 'ENSG00000170581',
+                            'ENSG00000173757', 'ENSG00000068078', 'ENSG00000175505', 'ENSG00000171150',
+                            'ENSG00000142166', 'ENSG00000108691', 'ENSG00000259384', 'ENSG00000164509',
+                            'ENSG00000138378', 'ENSG00000033800', 'ENSG00000118762']
+
+SAMPLE_ENSEMBL_GENE_LIST2 = ['ENSG00000196365', 'ENSG00000140451', 'ENSG00000198836', 'ENSG00000117020',
+                             'ENSG00000068305', 'ENSG00000068305', 'ENSG00000275199', 'ENSG00000151729',
+                             'ENSG00000114120', 'ENSG00000154719']
+
+
+def test_adjust_pvalue():
+    sample_p_values = [0.001, 0.008, 0.039, 0.041, 0.042, 0.06, 0.074, 0.205, 0.212, 0.216, 0.222, 0.251, 0.269,
+                       0.275, 0.34, 0.341, 0.384, 0.569, 0.594, 0.696, 0.762, 0.94, 0.942, 0.975, 0.986]
+
+    bh_corrected_p_values = [0.025, 0.1, 0.21, 0.21, 0.21, 0.25, 0.2643, 0.4911, 0.4911, 0.4911, 0.4911, 0.4911,
+                             0.4911, 0.4911, 0.5328, 0.5328, 0.5647, 0.7816, 0.7816, 0.87, 0.9071, 0.986, 0.986,
+                             0.986, 0.986]
+    numpy.testing.assert_almost_equal(atg.stats.enrich.p_adjust_bh(sample_p_values), bh_corrected_p_values,
+                                      decimal=4)
+
+    bh_corrected_p_values_n100 = atg.stats.enrich.p_adjust_bh(sample_p_values, n=100)
+    numpy.testing.assert_array_less(bh_corrected_p_values, bh_corrected_p_values_n100)
+
+    bh_corrected_p_values_n25 = atg.stats.enrich.p_adjust_bh(sample_p_values, n=25)
+    numpy.testing.assert_almost_equal(bh_corrected_p_values, bh_corrected_p_values_n25, decimal=4)
+
+class EnrichmentTest(unittest.TestCase):
+    def setUp(self):
+        data_root = os.path.expanduser(atg.config.settings['Data']['Root'])
+        go_term_path = os.path.join(data_root, 'human', 'Current', 'hg38', 'gene_go.csv')
+        go_definition_path = os.path.join(data_root, 'human', 'Current', 'hg38', 'go_definition.csv')
+        self.calculator = atg.stats.enrich.EnrichmentCalculator(go_term_path, go_definition_path)
+        self.all_genes = self.calculator.gene_term_df['Ensembl Gene ID'].drop_duplicates()
+
+    def test_single_term(self):
+        good_gene_list = SAMPLE_ENSEMBL_GENE_LIST[0:5] + self.all_genes.sample(100).tolist()
+        good_pvalue = self.calculator.get_single_enrichment(good_gene_list, 'GO:0007259')
+
+        better_gene_list = SAMPLE_ENSEMBL_GENE_LIST[0:10] + self.all_genes.sample(100).tolist()
+        better_pvalue = self.calculator.get_single_enrichment(better_gene_list, 'GO:0007259')
+        self.assertLess(better_pvalue, good_pvalue)
+        self.assertLess(good_pvalue, -5.0)
+
+        bad_pvalue = self.calculator.get_single_enrichment(good_gene_list, 'GO:0000122')
+        self.assertGreater(bad_pvalue, -5.0)
+
+    def test_full_enrichment(self):
+        good_gene_list = SAMPLE_ENSEMBL_GENE_LIST[0:5] + self.all_genes.sample(100).tolist()
+
+        enrichment_df = self.calculator.get_all_enrichment(good_gene_list)
+        self.assertIn('GO:0007259', enrichment_df.index)
+        self.assertLess(sum(enrichment_df['log_pvalue'] < -5), 100)
+
+    def test_plot(self):
+        good_gene_list = SAMPLE_ENSEMBL_GENE_LIST[0:15] + SAMPLE_ENSEMBL_GENE_LIST2
+        self.calculator.plot_enrichment_single(good_gene_list)
+
+    def test_iterative_enrichment(self):
+        self.calculator.iterative_enrichment(SAMPLE_ENSEMBL_GENE_LIST + SAMPLE_ENSEMBL_GENE_LIST2 +
+                                             self.all_genes.sample(100).tolist())
