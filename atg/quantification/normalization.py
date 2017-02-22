6ba49bd120b9f10c1e55889fb533fe836134c8c7
Max Chang <maxwchang@gmail.com>
2017-02-22 10:33:02 -0800

    suppress statsmodels warning

--- atg/quantification/normalization.py
+++ atg/quantification/normalization.py
@@ -3,6 +3,7 @@ import numpy as np
 import statsmodels
 import statsmodels.api as sm
 import statsmodels.formula.api as smf
+import warnings
 
 
 DEFAULT_ASYMPTOTIC_DISPERSION = 0.01
@@ -183,8 +184,11 @@ def _fit_vst_parameters(count_df, sample_size=1000, min_mean_count=5.0, min_disp
     dispersion_fit_df = dispersion_for_fit.to_frame(name="dispersion").join(normalized_count_df.ix[:, 'baseMean'])
 
     formula = 'dispersion ~ I(1/baseMean)'
-    model = smf.glm(formula=formula, data=dispersion_fit_df,
-                    family=sm.families.Gamma(link=statsmodels.genmod.families.links.identity)).fit()
+    with warnings.catch_warnings():
+        warnings.filterwarnings('ignore', message="The identity link function does not respect the domain of the "
+                                                  "Gamma family.")
+        model = smf.glm(formula=formula, data=dispersion_fit_df,
+                        family=sm.families.Gamma(link=statsmodels.genmod.families.links.identity)).fit()
 
     return model.params
 
